d923729a942c4c0e2695ab9077cef2f8aa9f43b5
Samuel Yvon <samuelyvon9@gmail.com>
2018-04-25 22:59:33 -0400

    Started the insertion tests

--- pymaps/tests/test_binary_search_trees.py
+++ pymaps/tests/test_binary_search_trees.py
@@ -8,3 +8,45 @@ class TestBinarySearchTrees(unittest.TestCase):
     def test_create_tree(self):
         bt = BinarySearchTree()
         self.assertEqual(len(bt), 0)
+
+    def test_basic_functions(self):
+        bt = BinarySearchTree()
+
+        bt[5] = 1
+
+        self.assertEqual(len(bt), 1)
+        self.assertEqual(bt._root.get_key(), 5)
+        self.assertEqual(bt._root.get_value(), 1)
+
+        bt[4] = 2
+
+        self.assertEqual(bt._root.get_child().get_key(), 4)
+
+        self.assertEqual(bt.get_max(), (5, 1))
+        self.assertEqual(bt.get_min(), (4, 2))
+
+        bt[6] = 3
+
+        self.assertEqual(bt._root.get_child(False).get_key(), 6)
+
+    def test_predecessor(self):
+        bt = BinarySearchTree()
+        bt[1] = 1
+        bt[2] = 2
+        bt[3] = 3
+        bt[-1] = -1
+        bt[4] = 4
+
+        start = bt._max_node
+        self.assertEqual(start.get_key(), 4)
+        walk = bt._inorder_predecessor(start)
+        self.assertEqual(walk.get_key(), 3)
+
+        walk = bt._inorder_predecessor(walk)
+        self.assertEqual(walk.get_key(), 2)
+
+        walk = bt._inorder_predecessor(walk)
+        self.assertEqual(walk.get_key(), 1)
+
+        walk = bt._inorder_predecessor(walk)
+        self.assertEqual(walk.get_key(), -1)
--- pymaps/trees/BinarySearchTree.py
+++ pymaps/trees/BinarySearchTree.py
@@ -1,6 +1,3 @@
-from abc import ABC
-
-
 class TreeNode:
     __slots__ = ["_key", "_value", "_parent_node", "_left_child", "_right_child"]
 
@@ -34,6 +31,12 @@ class TreeNode:
         self._value = new_value
         return old_value
 
+    def get_parent(self):
+        return self._parent_node
+
+    def __repr__(self):
+        return f"[{self._key}:{self._value}]"
+
 
 LEFT = True
 RIGHT = False
@@ -48,17 +51,37 @@ class BinarySearchTree:
         self._min_node = None
         self._max_node = None
 
+    def _inserted_hook(self, inserted_node):
+        pass
+
+    def _accessed_hook(self, accessed_node):
+        pass
+
+    def _deleted_hook(self, parent_node):
+        pass
+
     def _search(self, key):
         last = None
         walk = self._root
 
         while walk is not None and walk.get_key() != key:
             last = walk
-            walk = walk.get_child(walk.get_key() < key)
+            walk = walk.get_child(key < walk.get_key())
 
         return walk if walk is not None else last
 
+    def __setitem__(self, key, value):
+        self._insert(key, value)
+
     def _insert(self, key, value):
+        """
+        Insert a node inside the tree. It will perform a search and then insert the node
+        inside the tree.
+        :performance: O(h), where the h is the height of the tree
+        :param key: the key of the mapping
+        :param value: the value of the mapping
+        :return: void
+        """
 
         insertion_spot = self._search(key)
 
@@ -67,6 +90,7 @@ class BinarySearchTree:
             node = self._root
         elif insertion_spot.get_key() == key:
             insertion_spot.set_value(value)
+            self._accessed_hook(insertion_spot)
             node = None
         elif insertion_spot.get_key() < key:
             right_child = self._make_node(key, value, insertion_spot)
@@ -81,21 +105,41 @@ class BinarySearchTree:
             self._item_count += 1
             if self._min_node is None or key < self._min_node.get_key():
                 self._min_node = node
-            if self._min_node is None or key > self._max_node.get_key():
+            if self._max_node is None or key > self._max_node.get_key():
                 self._max_node = node
+            self._inserted_hook(node)
 
     def _remove(self, key, value):
-        pass
+        pass  # TODO
 
+    def _inorder_traversal(self, node=None):
+
+        if node is None:
+            node = self._root
+
+        yield from self._inorder_traversal(node.get_child(LEFT))
+        yield node
+        yield from self._inorder_traversal(node.get_child(RIGHT))
+
+    # noinspection PyMethodMayBeStatic
     def _inorder_predecessor(self, node):
-
+        """
+        Find the inorder predecessor than of the node. That is, the node with the largest smallest key than
+        the node's itself
+        :param node: the node from where we start
+        :return: the predecessor
+        """
+        # TODO write test, and check algorithm
         if node.has_child(left=True):
             walk = node.get_child()
             while walk.has_child(RIGHT):
                 walk = walk.get_child(RIGHT)
             return walk
         else:
-            pass  # TODO parent with lt key
+            walk = node.get_parent()
+            while walk is not None and walk.get_key() > node.get_key():
+                walk = walk.get_parent()
+            return walk
 
     def _inorder_successor(self, node):
         pass
@@ -104,4 +148,33 @@ class BinarySearchTree:
         return self._item_count
 
     def _make_node(self, key, value, parent):
+        """
+        Make a node in the tree. Can be overriden by subclasses
+        :param key: the key
+        :param value: the value
+        :param parent: the parent node, can be None if root
+        :return:
+        """
         return TreeNode(key, value, parent)
+
+    def get_max(self):
+        """
+        Get the key and the value associated with the largest key
+        :performance O(1)
+        :return: tuple (key, value)
+        """
+        if self._item_count == 0:
+            raise ValueError("Empty Binary Search tree")
+        else:
+            return self._max_node.get_key(), self._max_node.get_value()
+
+    def get_min(self):
+        """
+        Get the key and the value associated with the smallest key
+        :performance O(1)
+        :return: tuple (key, value)
+        """
+        if self._item_count == 0:
+            raise ValueError("Empty Binary Search tree")
+        else:
+            return self._min_node.get_key(), self._min_node.get_value()
